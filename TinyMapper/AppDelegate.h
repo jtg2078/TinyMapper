c5ef75d1ed8be8268edd632f58cf44df04df6b6c
Jason Ling <jtg2078@gmail.com>
2012-06-19 22:53:51 +0800

    initial commit

---
+++ TinyMapper/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TinyMapper
+//
+//  Created by ling tsu hsuan on 6/19/12.
+//  Copyright (c) 2012 jtg2078@hotmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TinyMapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TinyMapper
+//
+//  Created by ling tsu hsuan on 6/19/12.
+//  Copyright (c) 2012 jtg2078@hotmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
